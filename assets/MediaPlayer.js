d43a57d1c01ca0ea32378132672a1383731dc00c
Honguito <eduardolalojami@gmail.com>
2021-09-17 15:03:41 -0500

    plugins y mute, reto terminado

--- assets/MediaPlayer.js
+++ assets/MediaPlayer.js
@@ -2,6 +2,14 @@
 class MediaPlayer{
     constructor(config) {
         this.media = config.el;
+        this.plugins = config.plugins || [];
+
+        this._initPlugins()
+    }
+    _initPlugins(){
+        this.plugins.forEach(plugin => {
+            plugin.run(this)
+        });
     }
     play() {
         this.media.play();
@@ -13,6 +21,15 @@ class MediaPlayer{
         this.media.paused === false 
             ? this.media.pause()
             : this.media.play()
+    }
+    mute(){
+        this.media.muted = true
+    }
+    unmute(){
+        this.media.muted = false
+    }
+    toggleMute(){
+        this.media.muted = !this.media.muted;
     }
 }
 
--- assets/index.js
+++ assets/index.js
@@ -1,10 +1,20 @@
 import MediaPlayer from "./MediaPlayer.js";
+import AutoPlay from "./plugins/AutoPlay.js";
 
 const video = document.querySelector("video");
+const player = new MediaPlayer({
+    el: video, 
+    plugins: [
+        // new AutoPlay()
+    ]
+});
+
+
 const button = document.querySelector("button");
+const muteButton = document.getElementById("mute-botton");
 
 
-const player = new MediaPlayer({el: video});
 
 button.onclick = () => player.togglePlay();
+muteButton.onclick = () => player.toggleMute();
 
