120fd5e50887ffe1ff08df62d439a82f4bde92d3
Honguito <eduardolalojami@gmail.com>
2021-09-20 19:46:16 -0500

    convert AutoPause.js into AutoPause.ts

--- assets/plugins/AutoPause.ts
+++ assets/plugins/AutoPause.ts
@@ -1,4 +1,9 @@
+import MediaPlayer from "../MediaPlayer";
+
 class AutoPause {
+    private threshold: number;
+    private player: MediaPlayer;
+
     constructor(){
         this.threshold = 0.35;
         this.handleIntersection = this.handleIntersection.bind(this)
@@ -12,7 +17,7 @@ class AutoPause {
         observer.observe(this.player.media);
         document.addEventListener('visibilitychange', this.handleVisibilityChange);
     };
-    handleIntersection(entries){
+    private handleIntersection(entries: IntersectionObserverEntry[]){
         const entry = entries[0];
         
         const isVisible = entry.intersectionRatio >= this.threshold;
@@ -23,7 +28,7 @@ class AutoPause {
             this.player.pause();
         }
     }
-    handleVisibilityChange(){
+    private handleVisibilityChange(){
         const isVisible = document.visibilityState === "visible";
         if(isVisible){
             this.player.play();
