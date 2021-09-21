ebe0a7fe9a690677e4b287eea1108e9051cab1d7
Honguito <eduardolalojami@gmail.com>
2021-09-21 15:33:13 -0500

    terminando el proyecto, ads y video

--- assets/MediaPlayer.ts
+++ assets/MediaPlayer.ts
@@ -6,12 +6,19 @@ interface ConfigParam {
 class MediaPlayer{
     media: HTMLMediaElement;
     plugins: Array<any>;
+    container: HTMLElement;
 
     constructor(config: ConfigParam) {
         this.media = config.el;
         this.plugins = config.plugins || [];
-
+        this.initPlayer()
         this.initPlugins()
+    }
+    initPlayer(){
+        this.container = document.createElement('div')
+        this.container.style.position = 'relative';
+        this.media.parentNode.insertBefore(this.container, this.media);
+        this.container.appendChild(this.media)
     }
     private initPlugins(){
         this.plugins.forEach(plugin => {
